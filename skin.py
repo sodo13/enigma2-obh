4d2ce5afb7c541ec0ece18b4f3e2c54d4a01f953
sodo13 <rafika13@outlook.com>
2015-06-12 18:24:23 +0100

    Update skin.py

--- skin.py
+++ skin.py
@@ -75,14 +75,15 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "MX_HQ7/skin.xml"
+DEFAULT_SKIN = "skin.xml"
 # on SD hardware, PLi-HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	# in that case, fallback to Magic (which is an SD skin)
 	DEFAULT_SKIN = "Magic/skin.xml"
+config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)	
 #forced skin default by black hole
-DEFAULT_SKIN = "skin.xml"
-config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
+#DEFAULT_SKIN = "skin.xml"
+#config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
 
 profile("LoadSkin")
 res = None
